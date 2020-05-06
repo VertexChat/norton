3e0b6d6193f65ea8f4c1ce0dc9e1a6467c3aa47d
Cathal <cathalbutler@outlook.com>
2020-05-06 13:32:16 +0100

    Updated config file and requirments.txt (#7)
    * README.md update
    
    * Changed from MySqlConnector to Sqlalchemy for handing queries to the databases, this will also allow for the future use of different databases like postgresql, sqlite and Microsoft SQL.
    
    + Added generated table classes
    * Updated auth_controller.py & user_controller.py to now use SqlAlchemy
    
    * Updated login response to now include user details who logged in
    
    * Updated requirements.txt
    
    * Updated channel_controller.py to now include a getAllChannels function, Updated requirements.txt
    
    * channel_controller.py now have a function that will return all channels to the client from the database
    + Added a new model all_channels.py which is used when returning all the channels to the client
    * Updated swagger.yaml to include this new route
    * Updated requirements.txt as it has new dependencies
    
    * Fixed a data type in the OpenAPI spec that was wrong for channel model
    
    * Updated OpenAPI spec to fix datatype for user_id in the channel model.
    * Changed running port to 8081.
    * Added server config file to repo for convenience when deploying on server.
    
    * Removed server definition from OpenAPI spec
    
    * Updated config.py and requirements.txt

--- openapi_server/__main__.py
+++ openapi_server/__main__.py
@@ -2,6 +2,7 @@
 from flask_sqlalchemy import SQLAlchemy
 import connexion
 from sqlalchemy.exc import SQLAlchemyError
+from flask_cors import CORS
 
 import config
 from openapi_server import encoder
@@ -11,6 +12,8 @@ db = SQLAlchemy()
 
 def main():
     app = connexion.App(__name__, specification_dir='./openapi/')
+    # CORS(app.app, supports_credentials=True, origin="http://localhost:36859/")  # Added to allow CORS
+    CORS(app.app)
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('openapi.yaml', arguments={'title': 'Norton'}, pythonic_params=True)
 
